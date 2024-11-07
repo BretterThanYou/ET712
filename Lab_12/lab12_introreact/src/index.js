e566fa774fd0aeaf86c7478019de7148dbcc8405
BretterThanYou <BrettHirsch0@gmail.com>
2024-11-07 15:42:12 -0500

    Completed Lab12

--- Lab_12/lab12_introreact/src/index.js
+++ Lab_12/lab12_introreact/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import car from './images/car.jpg'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//content in the app
+const App = function(){
+  // creating a JSX variable
+  const anymsg = "I am text consant variable"
+  return (
+    <>
+    <h1 style={{color:'magenta', textAlign:'center'}}>Welcome to React JS</h1>
+    <h2 style={{fontFamily:'algerian'}} className='subtitle'>Brett Hirsch</h2>
+    <figure style={{border:"inset olive 5px", width: '60%', margin:'auto'}}>
+      <img src={car} style={{width:'100%'}}/>
+    </figure>
+    <h2>{anymsg}</h2>
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+App()
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
