f7abb99e513943903189588a958549a7cf7a4136
BretterThanYou <157437074+BretterThanYou@users.noreply.github.com>
2024-12-03 21:02:28 +0000

    Started lab16 react form states

---
+++ Lab_16/lab16_formstate/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  //OPEN AND CLOSE BUTTON
+  const [isOpen, setIsOpen] = useState(true)
+
+  //Function to toggle the isOpen state
+  const openreadmore = function(){
+    setIsOpen(!isOpen)
+  }
+  return (
+    <div className="App">
+    <h1>Brett Hirsch</h1>
+      <div className='info'>
+        <p><b>Cat (Felis catus)</b>, commonly referred to as the domestic pet or house cat</p>
+        <button className='readmorebtn' onClick={openreadmore}>Read more</button>
+      </div>
+      <section>
+        {
+          isOpen &&
+        <div className='readmore'>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        }
+      </section>
+    </div>
+  );
+}
+
+export default App;
